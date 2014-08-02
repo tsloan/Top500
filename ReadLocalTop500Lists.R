c16416ef011d2df38484bcf3ac08e3570b4dcce2
tsloan <terry@sloandrums.com>
2014-08-02 13:01:46 +0100

    reads local files

---
+++ ReadLocalTop500Lists.R
@@ -0,0 +1,51 @@
+#############################################################################
+##
+## ReadLocalTop500Lists.R 
+##
+## This script reads locally stored copies of Top500 supercomputer lists
+## previously downloadedfrom the Supercomputer Top 500 site.
+##
+################################################################################  
+
+source("Top500-Helper.R") # helper functions
+
+start.year <- 1993 # Initial year to read the Top 500 list 
+end.year <- 1993 # Final year to read the Top 500 list
+data.dir <- "data-Top500-lists"  # local directory where the lists are stored
+
+ReadTop500file <- function(Top500filename){
+    if (file.exists(Top500filename)){
+        df<-read.csv(Top500filename, stringsAsFactors=FALSE)
+        ## need to add a column containing the list year and month
+        ## assumes Top500filename is of format YYYYMM
+        df$list.year<-substr(Top500filename,1,4)
+        df$list.month<-substr(Top500filename,5,6)
+        df
+    }
+}
+
+##############################################################################
+##
+## ~ReadTop500Lists
+##
+##############################################################################
+ReadTop500Lists<-function(dir, start.year, end.year){
+    work.dir<-getwd()
+    setwd(dir)
+    ## create the names of the files containing the Top500 lists
+    Top500FilenameList <- CreateFileList(start.year, end.year)
+    
+    top500.df<-lapply(Top500FilenameList, ReadTop500file)
+    setwd(work.dir)
+    #########################################################################
+    ## return the list of data frames containing the contents
+    ## of the Top500 files
+    
+    top500.df 
+         
+}
+
+# test run
+#top500.df<-ReadTop500Lists(paste("./",data.dir,sep=""),
+#                           start.year,end.year)
+
